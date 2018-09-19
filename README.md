fd1fe747404cead5aaaa4a1f15cef58ac90df743
Etienne <34507362+Master-Goldeau@users.noreply.github.com>
2018-09-20 00:15:24 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# ReactStarter_Users
-Tuto with Redux
+### ReactRedux-Starter ###
+
+Pour installer le projet se placer dans le projet et lancer : 
+
+```
+> npm install
+> npm start
+```
+
---
+++ src/reducers/reducer_users.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { name: "Sarah Croche", id: 1, role: "ROOT", active: "NON" },
+    { name: "Alain Térieur", id: 2, role: "USER", active: "OUI" },
+    { name: "Jean Bonnot", id: 3, role: "USER", active: "NON" },
+    { name: "Eva Zipoupou", id: 4, role: "ADMIN", active: "OUI" }
+  ];
+}
